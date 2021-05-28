58b79f5c796d68332626fd3821bbee9860597813
Saaheel Majethia <saaheelm@gmail.com>
2021-05-28 09:40:12 +0530

    using use middleware

--- node/academind/with expressjs/app.js
+++ node/academind/with expressjs/app.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In middleware");
-    next();     // allows the request to contunue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    // console.log("In product middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add</button></form>') //html content type
+
 })
-app.use((req, res, next) => {
-    console.log("In another middleware");
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/users', (req, res, next) => {
+    // console.log('in user middleware');
+    res.send('<h1>See Users</h1>') 
+})
+
+
+app.use('/', (req, res, next) => {
+    // console.log("In home middleware");
     res.send('<h1>Hello Express</h1>') //html content type
-    // res.send(true)  //json content type
 })
 
 
