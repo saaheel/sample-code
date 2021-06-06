b934674097fe4c11108c8512d909c2c54c8b7c28
Saaheel Majethia <saaheelm@gmail.com>
2021-06-06 19:49:03 +0530

    Create Pug Template files

--- node/academind/nodejs-withexpressjs/app.js
+++ node/academind/nodejs-withexpressjs/app.js
@@ -5,18 +5,22 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(shopRoutes)
-app.use('/admin',adminRoutes)
+app.use('/admin',adminData.routes)
 
 app.use((req, res, next) => {
     // res.status(404).send('<h1>Page note found</h1>')
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))      // HTML file 
+    res.status(404).render('404')
 })
 
 app.listen(3000)
--- node/academind/nodejs-withexpressjs/routes/admin.js
+++ node/academind/nodejs-withexpressjs/routes/admin.js
@@ -6,17 +6,22 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product       => GET
 router.get('/add-product', (req, res, next) => {
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button>Add</button></form>') //html content type
     // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))       // render html
+    res.render('add-product', {pageTitle: 'Add Product'})                                        // render pug template
 })
 
 // /admin/add-product       => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    // console.log(req.body);
+    products.push({ title: req.body.title })
     res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
