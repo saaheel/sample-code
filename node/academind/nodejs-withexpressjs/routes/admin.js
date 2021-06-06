43fadcf6b1f652065055f445587133675617cd6c
Saaheel Majethia <saaheelm@gmail.com>
2021-06-06 22:50:08 +0530

    Add handlebars template engine

--- node/academind/nodejs-withexpressjs/routes/admin.js
+++ node/academind/nodejs-withexpressjs/routes/admin.js
@@ -13,7 +13,13 @@ router.get('/add-product', (req, res, next) => {
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button>Add</button></form>') //html content type
     // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))       // render html
-    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})                                        // render pug template
+    res.render('add-product', {
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        formsCSS: true,
+        productCSS: true
+    })                                        // render pug template
 })
 
 // /admin/add-product       => POST
