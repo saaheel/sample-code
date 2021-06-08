ed0883ce8bfa2bd9cc7cd79d7183098b1907a850
Saaheel Majethia <saaheelm@gmail.com>
2021-06-08 13:09:38 +0530

    Added controllers

--- node/academind/nodejs-withexpressjs/routes/admin.js
+++ node/academind/nodejs-withexpressjs/routes/admin.js
@@ -2,32 +2,16 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-const products = [];
+
 
 // /admin/add-product       => GET
-router.get('/add-product', (req, res, next) => {
-    // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button>Add</button></form>') //html content type
-    // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
-    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))       // render html
-    res.render('add-product', {
-        pageTitle: 'Add Product', 
-        path: '/admin/add-product',
-        activeAddProduct: true,
-        formsCSS: true,
-        productCSS: true
-    })                                        // render pug template
-})
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product       => POST
-router.post('/add-product', (req, res, next) => {
-    // console.log(req.body);
-    products.push({ title: req.body.title })
-    res.redirect('/');
-})
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
