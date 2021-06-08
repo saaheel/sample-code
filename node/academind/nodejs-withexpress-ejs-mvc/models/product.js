bbea1031241e388a1f635744ab9e455cca639c52
Saaheel Majethia <saaheelm@gmail.com>
2021-06-08 15:51:10 +0530

    Separate code for mvc

---
+++ node/academind/nodejs-withexpress-ejs-mvc/models/product.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+        
+    });
+}
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        })
+    // const p = path.join(
+    //     path.dirname(process.mainModule.filename),
+    //     'data',
+    //     'products.json'
+    // );
+    // fs.readFile(p, (err, fileContent) => {
+    //     let products = [];
+    //     if (!err) {
+    //         products = JSON.parse(fileContent);
+    //     }
+    //     products.push(this);
+    //     fs.writeFile(p, JSON.stringify(products), err => {
+    //         console.log(err);
+    //     });
+    // });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
